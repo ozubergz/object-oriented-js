bd0bea50fa161dc08e314d0bd78361c092bb5497
ozubergz <jmin499@gmail.com>
2020-11-15 00:20:35 -0500

    first class

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-//object
-const user = {
-    email: 'hello@world.com',
-    name: 'Ryu',
-    login() {
-        console.log(this.email, 'has logged in.')
-    },
-    logout() {
-        console.log(this.email, 'has logged out.')
-    }
-}
+//////////////////////// Object //////////////////////////////////
 
-console.log(user.email)
-console.log(user.login())
-console.log(user.logout())
+// const user = {
+//     email: 'hello@world.com',
+//     name: 'Ryu',
+//     login() {
+//         console.log(this.email, 'has logged in.')
+//     },
+//     logout() {
+//         console.log(this.email, 'has logged out.')
+//     }
+// }
+
+// console.log(user.email)
+// console.log(user.name)
+// console.log(user.login())
+// console.log(user.logout())
+
+//////////////////////// Class //////////////////////////////////
+
+
+
