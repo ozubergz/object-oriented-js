897c06e493db00f1eb75a5bb4350e474af1762d1
ozubergz <jmin499@gmail.com>
2020-11-16 21:17:47 -0500

    Prototype

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ function User(email, name) {
     this.email = email;
     this.name = name;
     this.online = false;
-    this.login = function() {
-        console.log(this.email, 'has logged in')
-    }
+    // this.login = function() {
+    //     console.log(this.email, 'has logged in')
+    // }
+}
+
+User.prototype.login = function() {
+    this.online = true;
+    console.log(this.email, 'has logged in')
+}
+
+User.prototype.logout = function() {
+    this.online = false;
+    console.log(this.email, 'has logged out')
 }
 
 const userOne = new User('hello@world.com', 'Ryu');
 const userTwo = new User('jim@world.com', 'Jim');
 
 console.log(userOne);
-userTwo.login();
-
